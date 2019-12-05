180847ed1049b551c3f97f904d9d4e6d827a9014
Guodong Chen <cgderic@umich.edu>
2019-12-05 17:55:16 -0500

    add consts header file

---
+++ DG_Const.cuh
@@ -0,0 +1,26 @@
+#ifndef _DG_Const_
+#define _DG_Const_
+
+// All the global constants used in the codes 
+
+const double PI = 3.141592653589793238;
+const double U_ABS = 1.0;
+const double U_INF = 0.7071067811865476;
+const double V_INF = 0.7071067811865476;
+const double RHO_INF = 1.0;
+const double M_INF = 0.5;
+const double T0 = 0.0;
+const double Tf = 14.142135623730951; 
+const double P_INF = 2.857142857142857; 
+const double GAMMA = 1.4;
+const double EPSILON = 0.3;
+const double RC = 1.0; 
+const int NUM_OF_STATES = 4; 
+const double X_ORIGINAL[2] = {0.0, 0.0};
+const double SQRT2 = 1.4142135623730951;
+const int MAX_CHAR_LENGTH = 100;
+
+
+
+
+#endif 
