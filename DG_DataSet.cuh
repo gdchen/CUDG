a575afcaf8349ccdae066ca24762fa8f24be5904
Guodong Chen <cgderic@umich.edu>
2019-12-07 15:24:43 -0500

    update DataSet

--- DG_DataSet.cuh
+++ DG_DataSet.cuh
@@ -74,67 +74,6 @@ computeMassMatrix(DG_DataSet *DataSet, const DG_Mesh *Mesh, const DG_BasisData *
 cudaError_t interpolateIC(DG_DataSet *DataSet, const DG_Mesh *Mesh);
 
 
-// initialization with least square projection 
-//void lsqIC(DG_DataSet *DataSet, const DG_Mesh *Mesh, const DG_BasisData *BasisData)
-//{
-//  int nElem = DataSet->nElem;
-//  int np = BasisData->np;
-//  int nq2 = BasisData->nq2; 
-//  double *Phi = BasisData->Phi;
-//  double *wq2 = BasisData->wq2;
-//  double *Dwq2 = (double *)malloc(nq2*nq2*sizeof(double));
-//  int n, i, j; 
-//  for (i=0; i<nq2; i++) {
-//    for (j=0; j<nq2; j++) Dwq2[i*nq2+j] = 0;
-//    Dwq2[i*nq2+i] = wq2[i]; 
-//  }
-//  // Initialize State
-//  DataSet->State = (double **)malloc(nElem*sizeof(double *));  
-//  for (n=0; n<nElem; n++){
-//    DataSet->State[n] = (double *)malloc(np*NUM_OF_STATES*sizeof(double));
-//    for (i=0; i<np*NUM_OF_STATES; i++) DataSet->State[n][i] = 0; 
-//  }
-//
-//
-//  double *b = (double *)malloc(np*NUM_OF_STATES*sizeof(double));
-//  for (i=0; i<np*NUM_OF_STATES; i++) b[i] = 0;
-//  double *u_exact = (double *)malloc(nq2*NUM_OF_STATES*sizeof(double)); 
-//  for (i=0; i<nq2*NUM_OF_STATES; i++) u_exact[i] = 0; 
-//  // u_exact 
-//  double **xyGlobal = (double **)malloc(nElem*nq2*sizeof(double *));
-//  for (i=0; i<nElem*nq2; i++) xyGlobal[i] = (double *)malloc(2*sizeof(double));
-//  getGlobalQuadPoints(xyGlobal, Mesh, BasisData);
-//  double f0, f1, f2, rho, u, v, p;
-//  double *temp = (double *)malloc(np*nq2*sizeof(double));
-//  for (i=0; i<np*nq2; i++) temp[i] = 0; 
-//  
-//  for (i=0; i<nElem; i++){
-//    for (j=0; j<nq2; j++){
-//      f0 = getf0(xyGlobal[i*nq2+j]);
-//      f1 = getf1(f0);
-//      f2 = getf2(f0);
-//      rho = RHO_INF*pow(f1, 1.0/(GAMMA-1));
-//      u = U_INF - f2*(xyGlobal[i*nq2+j][1]-X_ORIGINAL[1]);
-//      v = V_INF + f2*(xyGlobal[i*nq2+j][0]-X_ORIGINAL[0]);
-//      p = P_INF*pow(f1, GAMMA/(GAMMA-1));
-//      u_exact[j*NUM_OF_STATES+0] = rho;
-//      u_exact[j*NUM_OF_STATES+1] = rho*u;
-//      u_exact[j*NUM_OF_STATES+2] = rho*v;
-//      u_exact[j*NUM_OF_STATES+3] = p/(GAMMA-1) + 0.5*rho*(u*u+v*v);
-//    }
-//    DG_MTxM_Set(np, nq2, nq2, Phi, Dwq2, temp);
-//    DG_cMxM_Set(Mesh->detJ[i], np, nq2, NUM_OF_STATES, temp, u_exact, b);  // Integration mapping
-//    DG_MxM_Set(np, np, NUM_OF_STATES, DataSet->InvMassMatrix[i], b, DataSet->State[i]);
-//  }
-//  for (i=0; i<nElem*nq2; i++) free(xyGlobal[i]); free(xyGlobal);
-//  free(Dwq2);  free(b); free(u_exact); free(temp);
-//
-//} 
-
-
-
-
-
 /* Function:
  * purpose : Get states at elemental interior Quad points 
  * inputs  : State     == elemental state vector 
