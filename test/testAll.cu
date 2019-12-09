ca5927dc9327878ee2b9a98b58d0cc47090b4a90
Guodong Chen <cgderic@umich.edu>
2019-12-09 13:22:00 -0500

    update testAll

--- test/testAll.cu
+++ test/testAll.cu
@@ -3,17 +3,21 @@
 #include "../DG_Mesh.cuh"
 #include "../DG_Basis.cuh"
 #include "../DG_DataSet.cuh"
-//#include "DG_Residual.cuh"
-//#include "DG_PostProcess.cuh"
+#include "../DG_Residual.cuh"
+#include "../DG_PostProcess.cuh"
 
 
 
 
 int main(int argc, char const *argv[])
 {
-  
+  size_t heapSize, a; 
+  heapSize = 1.0*1024*1024*1024/8.0 *sizeof(double); 
+  CUDA_CALL(cudaDeviceSetLimit(cudaLimitMallocHeapSize,heapSize)); 
+  CUDA_CALL(cudaDeviceGetLimit(&a, cudaLimitMallocHeapSize));
+  printf("Heap size = %ld, %ld\n", heapSize, a);
   int order = 1;
-  int N = 16; 
+  int N = 200; 
   double halfL = 5;
   DG_All *All;
   createAll(&All);
@@ -22,12 +26,13 @@ int main(int argc, char const *argv[])
   //int np = All->BasisData->np;
   //int n, i, j;
   //printf("%d %d %d \n", nElem, All->BasisData->nq1, All->BasisData->nq2);
-//  double dt = 0.001; 
-//  //int Nt = 1;
+  double dt = 0.001; 
+  int Nt = 10;
 //  int Nt = 5414;
 //  //int Nt = 14142;
-//  DG_Solve(All, dt, Nt);
-//  //writeStates(All);
+  DG_RK4(All, dt, Nt);
+  CUDA_CALL(cudaDeviceSynchronize());
+  writeStates(All);
 //  //writeStatesP(All,4);
 //  double eu, es, ep;
 //  ErrEst(All, &eu, &es, &ep);
